12937be0e3977c973dc89147e83a4e637451d640
Escobar <128872731+MW-Escobar@users.noreply.github.com>
2024-06-05 00:00:19 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const linkedin = document.getElementById('Linkedin');
+const instagram = document.getElementById('Instagram');
+
+        linkedin.addEventListener('mouseover', function() {
+            alert('LinkedIn: Conecte-se comigo!');
+        });
+
+        instagram.addEventListener('mouseover', function() {
+            alert('Instagram: Siga-me para mais atualizações!');
+        });
